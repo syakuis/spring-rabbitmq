93737fbaf85c373e7b91c8a1bd12b872b33bd79f
syaku <syaku@naver.com>
2021-10-17 22:06:19 +0900

    초기 설정

--- src/main/java/io/github/syakuis/config/BasicBeanConfiguration.java
+++ src/main/java/io/github/syakuis/config/BasicBeanConfiguration.java
@@ -6,12 +6,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import io.github.syakuis.config.support.SimpleObjectMapper;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.MessageSourceAccessor;
-
-import java.util.Locale;
 
 @Configuration
 public class BasicBeanConfiguration {
@@ -24,11 +20,5 @@ public class BasicBeanConfiguration {
         mapper.disable(MapperFeature.DEFAULT_VIEW_INCLUSION);
 
         return SimpleObjectMapper.of(mapper);
-    }
-
-    @Bean("i18n")
-    @ConditionalOnMissingBean
-    public MessageSourceAccessor i18n(MessageSource messageSource) {
-        return new MessageSourceAccessor(messageSource, Locale.getDefault());
     }
 }
--- src/main/java/io/github/syakuis/config/WebMvcConfiguration.java
+++ src/main/java/io/github/syakuis/config/WebMvcConfiguration.java
@@ -4,12 +4,10 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.core.env.Profiles;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.DelegatingWebMvcConfiguration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 import java.nio.charset.StandardCharsets;
 import java.util.List;
@@ -19,14 +17,6 @@ import java.util.List;
 public class WebMvcConfiguration extends DelegatingWebMvcConfiguration {
     private final Environment environment;
     private final ObjectMapper objectMapper;
-
-    // TODO restDocs setting
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        if (environment.acceptsProfiles(Profiles.of("stage | default"))) {
-            registry.addResourceHandler("/docs/**").addResourceLocations("classpath:/static/docs/");
-        }
-    }
 
     @Override
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
